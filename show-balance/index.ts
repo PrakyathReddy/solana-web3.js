8acf9def83332854c80073d177f6ea9335d30d8c
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-28 15:22:04 +0530

    referred to airdrop in show-balance module to update and retrieve balance

--- show-balance/index.ts
+++ show-balance/index.ts
@@ -1,4 +1,5 @@
 import { Connection, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { airdrop } from "../airdrop";
 
 export const showBalance = async (publicKey: PublicKey) => {
   const connection = new Connection("http://127.0.0.1:8899", "confirmed");
@@ -9,7 +10,8 @@ export const showBalance = async (publicKey: PublicKey) => {
 (async () => {
   const publicKey = "4VUsxdzEDc4qiFJSfiWuYQnFjQDsW2hXkevUGpCoweUX";
   const balance = await showBalance(new PublicKey(publicKey));
-  console.log(balance);
+  console.log(`The balance of the key ${publicKey} is ${balance} SOL`);
+  await airdrop(publicKey, 4);
+  const updatedBalance = await showBalance(new PublicKey(publicKey));
+  console.log(`updated balance is ${updatedBalance}`);
 })();
-
-// "The balance of the key ${publicKey} is ${balance} SOL"
